ceb44de44c1f00ee6bc72cf439443b3c2a50d541
nutony <52399178@qq.com>
2018-10-16 19:03:15 +0800

    jt-cart=test

---
+++ jt-cart/src/main/java/cn/tedu/jtcart/JtCartApplication.java
@@ -0,0 +1,12 @@
+package cn.tedu.jtcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtCartApplication.class, args);
+	}
+}
